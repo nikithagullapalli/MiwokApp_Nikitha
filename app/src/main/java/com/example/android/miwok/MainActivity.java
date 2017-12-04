70190ffaa9ccb08ca3b80fd09b7e506c4f977052
nikitha2 <gullapalli.nikitha@gmail.com>
2017-12-04 08:43:05 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.android.miwok;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set the content of the activity to use the activity_main.xml layout file
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openNumbersLsit(View view){
+        Intent numberinent = new Intent(this, NumbersActivity.class);
+        startActivity(numberinent);
+    }
+}
