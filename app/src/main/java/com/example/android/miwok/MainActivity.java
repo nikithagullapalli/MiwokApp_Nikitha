a043422935e7b428e81353d8b74be58c0fee26a9
nikitha2 <gullapalli.nikitha@gmail.com>
2017-12-05 08:42:58 -0500

    hi

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,10 +14,49 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
-    }
 
-    public void openNumbersLsit(View view){
-        Intent numberinent = new Intent(this, NumbersActivity.class);
-        startActivity(numberinent);
+        //Find the view that shows the number category
+        TextView numbers= (TextView)findViewById(R.id.numbers);
+        //set onclick Listener on the view
+        numbers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent numberIntent = new Intent(MainActivity.this, NumbersActivity.class);
+                startActivity(numberIntent);
+            }
+        });
+
+        //Find the view that shows the phrases category
+        TextView phrases= (TextView)findViewById(R.id.phrases);
+        //set onclick Listener on the view
+        phrases.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent phrasesIntent = new Intent(MainActivity.this, PhrasesActivity.class);
+                startActivity(phrasesIntent);
+            }
+        });
+
+        //Find the view that shows the familyMembers category
+        TextView family= (TextView)findViewById(R.id.family);
+        //set onclick Listener on the view
+        family.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent familyIntent = new Intent(MainActivity.this, FamilyMembersActivity.class);
+                startActivity(familyIntent);
+            }
+        });
+
+        //Find the view that shows the colors category
+        TextView colors= (TextView)findViewById(R.id.colors);
+        //set onclick Listener on the view
+        colors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent colorsIntent = new Intent(MainActivity.this, ColorsActivity.class);
+                startActivity(colorsIntent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,11 +3,16 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class NumbersActivity extends AppCompatActivity {
+    ArrayList<String> numbersEnglistList = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+
     }
 }
